24cb2cd54c78ca6fe507e5b79898f145058c5ff4
Reza <mrdev1020@gmail.com>
2024-01-28 21:09:42 +0330

    Sentiment buildAndTrainModel

---
+++ ML.NET/Microsoft/Sentiment_BinaryClassification/Program.cs
@@ -0,0 +1,40 @@
+﻿
+using Microsoft.ML;
+using Sentiment_BinaryClassification;
+using static Microsoft.ML.DataOperationsCatalog;
+
+string _dataPath = Path.Combine(Environment.CurrentDirectory, "Data", "yelp_labelled.txt");
+
+
+MLContext mLContext = new MLContext();
+TrainTestData splitDataView = LoadData(mLContext);
+
+ITransformer model = BuildAndTrainModel(mLContext, splitDataView.TrainSet);
+
+
+ITransformer BuildAndTrainModel(MLContext mlContext, IDataView splitTrainSet)
+{
+    var estimator = mLContext.Transforms.Text
+        .FeaturizeText(
+            outputColumnName: "Features",
+            inputColumnName: nameof(SentimentData.SentimentText))
+        .Append(mLContext.BinaryClassification.Trainers
+            .SdcaLogisticRegression(
+                labelColumnName: "Label",
+                featureColumnName: "Features"));
+
+    Console.WriteLine("=============== Create and Train the Model ===============");
+    var model = estimator.Fit(splitTrainSet);
+    Console.WriteLine("=============== End of training ===============");
+    Console.WriteLine();
+
+    return model;
+}
+
+TrainTestData LoadData(MLContext mlContext)
+{
+    IDataView dataView = mlContext.Data.LoadFromTextFile<SentimentData>(_dataPath, hasHeader: false);
+    TrainTestData splitDataView = mlContext.Data.TrainTestSplit(dataView, testFraction: 0.2);
+
+    return splitDataView;
+}
