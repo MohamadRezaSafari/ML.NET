5fc7eb38c32259d3d9800b80e89f554314952e21
Reza <mrdev1020@gmail.com>
2024-03-06 11:25:48 +0330

    Dividing text into sentences

--- Python/NLP - Cookbook/1 - Basics/test.py
+++ Python/NLP - Cookbook/1 - Basics/test.py
@@ -5,13 +5,25 @@ import spacy
 
 curerntPath = os.path.dirname(os.path.abspath(__file__))
 filename = os.path.join(curerntPath, 'sherlock_holmes_1.txt')
+# file = open(filename, "r", encoding="utf-8")
+
+# text = file.read()
+# text = text.replace("\n", " ")
+
+# nltk.download('punkt')
+# tokenizer = nltk.data.load("tokenizers/punkt/english.pickle")
+# sentences = tokenizer.tokenize(text)
+
+# print(sentences)
+
+
+
 file = open(filename, "r", encoding="utf-8")
-
 text = file.read()
-text = text.replace("\n", " ")
 
-nltk.download('punkt')
-tokenizer = nltk.data.load("tokenizers/punkt/english.pickle")
-sentences = tokenizer.tokenize(text)
+text = text.replace("\n", " ")
+nlp = spacy.load("en_core_web_sm")
+doc = nlp(text)
+sentences = [sentence.text for sentence in doc.sents]
 
 print(sentences)
