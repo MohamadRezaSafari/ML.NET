7f0ece8614e1ca65b8d29012a5667aaea2b53608
mrdev1020@gmail.com <mrdev1020@gmail.com>
2024-04-27 16:15:53 +0330

    feat: face mesh module

--- Python/Advanced Computer Vision with Python - Full Course/FaceMeshbasics.py
+++ Python/Advanced Computer Vision with Python - Full Course/FaceMeshbasics.py
@@ -12,7 +12,7 @@ pTime = 0
 mpDraw = mp.solutions.drawing_utils
 mpFaceMesh = mp.solutions.face_mesh
 faceMesh = mpFaceMesh.FaceMesh(max_num_faces=1)
-draqSpec = mpDraw.DrawingSpec(thickness=1, circle_radius=1)
+drawSpec = mpDraw.DrawingSpec(thickness=1, circle_radius=1)
 
 while True:
     success, img = cap.read()
@@ -20,7 +20,7 @@ while True:
     results = faceMesh.process(imgRGB)
     if results.multi_face_landmarks:
         for faceLms in results.multi_face_landmarks:
-            mpDraw.draw_landmarks(img, faceLms, mpFaceMesh.FACEMESH_TESSELATION, draqSpec, draqSpec)
+            mpDraw.draw_landmarks(img, faceLms, mpFaceMesh.FACEMESH_TESSELATION, drawSpec, drawSpec)
             for lm in faceLms.landmark:
                 print(lm)
 
